3e125657ddf177513cb62c10bddb9fa2e46db170
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-02-05 15:53:58 -0500

    Define and use config module

--- src/gemini_turbotax/__init__.py
+++ src/gemini_turbotax/__init__.py
@@ -6,6 +6,8 @@ import warnings
 import pandas as pd
 import fire
 
+import gemini_turbotax.config as config
+
 warnings.formatwarning = lambda message, category, filename, lineno, line=None: f'{category.__name__}: {message}\n'
 
 
@@ -28,19 +30,17 @@ def convert(input_path: str, output_path: Optional[str] = None) -> None:
         df_gemini.dropna(subset='Date', inplace=True)  # Removes last row which just has "USD Balance USD".
         warnings.warn(f'Skipped {num_gemini_na_rows} empty rows from the Gemini file, keeping {len(df_gemini)} rows.')
 
-    gemini_supported_types = ['Buy', 'Sell']
-    gemini_unsupported_types_mask = ~df_gemini['Type'].isin(gemini_supported_types)
+    gemini_unsupported_types_mask = ~df_gemini['Type'].isin(config.GEMINI_SUPPORTED_TYPES)
     num_gemini_unsupported_type_rows = gemini_unsupported_types_mask.sum()
     if num_gemini_unsupported_type_rows > 0:
         df_gemini.drop(df_gemini[gemini_unsupported_types_mask].index, inplace=True)
-        warnings.warn(f'Skipped {num_gemini_unsupported_type_rows} rows with unsupported types from the Gemini file, keeping {len(df_gemini)} rows. The supported types are: {", ".join(gemini_supported_types)}')
+        warnings.warn(f'Skipped {num_gemini_unsupported_type_rows} rows with unsupported types from the Gemini file, keeping {len(df_gemini)} rows. The supported types are: {", ".join(config.GEMINI_SUPPORTED_TYPES)}')
 
-    gemini_supported_symbol_suffix = 'USD'
-    gemini_unsupported_symbol_mask = ~df_gemini['Symbol'].str.endswith(gemini_supported_symbol_suffix)
+    gemini_unsupported_symbol_mask = ~df_gemini['Symbol'].str.endswith(config.GEMINI_SUPPORTED_SYMBOL_SUFFIX)
     num_gemini_unsupported_symbol_rows = gemini_unsupported_symbol_mask.sum()
     if num_gemini_unsupported_symbol_rows:
         df_gemini.drop(df_gemini[gemini_unsupported_symbol_mask].index, inplace=True)
-        warnings.warn(f'Skipped {num_gemini_unsupported_symbol_rows} rows with unsupported symbols from the Gemini file, keeping {len(df_gemini)} rows. Only symbols ending in {gemini_supported_symbol_suffix} are supported.')
+        warnings.warn(f'Skipped {num_gemini_unsupported_symbol_rows} rows with unsupported symbols from the Gemini file, keeping {len(df_gemini)} rows. Only symbols ending in {config.GEMINI_SUPPORTED_SYMBOL_SUFFIX} are supported.')
 
     # Validate Gemini dataframe
     if df_gemini.empty:
@@ -48,14 +48,14 @@ def convert(input_path: str, output_path: Optional[str] = None) -> None:
         return
 
     # Enhance Gemini dataframe
-    df_gemini['Symbol Prefix'] = df_gemini['Symbol'].str.removesuffix(gemini_supported_symbol_suffix)
-    df_gemini['Symbol Prefix Amount'] = df_gemini.apply(lambda row: row[row['Symbol Prefix'] + ' Amount ' + row['Symbol Prefix']], axis=1).round(8)
+    df_gemini['Symbol Prefix'] = df_gemini['Symbol'].str.removesuffix(config.GEMINI_SUPPORTED_SYMBOL_SUFFIX)
+    df_gemini['Symbol Prefix Amount'] = df_gemini.apply(lambda row: row[row['Symbol Prefix'] + ' Amount ' + row['Symbol Prefix']], axis=1).round(config.TURBOTAX_DECIMAL_PLACES)
 
     # Create TurboTax dataframe
     df_turbotax = pd.DataFrame()
     df_turbotax['Date'] = df_gemini['Time (UTC)']
     df_turbotax['Type'] = df_gemini['Type'].replace('Sell', 'Sale')
-    df_turbotax['Sent Asset'] = df_gemini['Type'].case_when([(lambda s: s.eq('Buy'), gemini_supported_symbol_suffix), (lambda s: s.eq('Sell'), df_gemini['Symbol Prefix'])])
+    df_turbotax['Sent Asset'] = df_gemini['Type'].case_when([(lambda s: s.eq('Buy'), config.GEMINI_SUPPORTED_SYMBOL_SUFFIX), (lambda s: s.eq('Sell'), df_gemini['Symbol Prefix'])])
     df_turbotax['Sent Amount'] = df_gemini['Type'].case_when([(lambda s: s.eq('Buy'), -df_gemini['USD Amount USD']), (lambda s: s.eq('Sell'), -df_gemini['Symbol Prefix Amount'])])
 
 
