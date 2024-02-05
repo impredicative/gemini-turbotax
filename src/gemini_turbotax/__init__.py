6babac3c20f1f1e45167377e2deb800b250ab874
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-02-05 09:20:32 -0500

    Move TurboTax CSV reference link into readme

--- src/gemini_turbotax/__init__.py
+++ src/gemini_turbotax/__init__.py
@@ -6,12 +6,16 @@ import fire
 
 
 def convert(input_path: str, output_path: Optional[str] = None) -> None:
-    # Ref: https://ttlc.intuit.com/turbotax-support/en-us/help-article/cryptocurrency/create-csv-file-unsupported-source/L1yhp71Nt_US_en_US
     input_path = pathlib.Path(input_path)
     if output_path is None:
-        output_path = input_path.parent / f'{input_path.stem}_gemini.csv'
+        output_path = input_path.parent / f'{input_path.stem}_turbotax.csv'
     print(f'Converting {input_path} to {output_path}.')
 
 
-def main() -> None:
+
+def main() -> None:  # Used as target by pyproject.toml
     fire.Fire(convert)
+
+
+if __name__ == '__main__':
+    main()
