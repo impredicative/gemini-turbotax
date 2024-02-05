8e54087f887e370a82c88d5fff41d6d15bc26417
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-02-04 22:58:12 -0500

    Add run target "convert"

--- src/gemini_turbotax/__init__.py
+++ src/gemini_turbotax/__init__.py
@@ -1,2 +1,9 @@
-def hello():
-    return "Hello from gemini-turbotax!"
+import fire
+
+
+def convert(input_path: str, output_path: str) -> None:
+    print(f'Converting {input_path} to {output_path}.')
+
+
+def main() -> None:
+    fire.Fire(convert)
