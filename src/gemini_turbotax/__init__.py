0d0f6179ef54898fa93595a5b37785bac6501750
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-02-04 22:59:27 -0500

    Make output_path optional

--- src/gemini_turbotax/__init__.py
+++ src/gemini_turbotax/__init__.py
@@ -1,7 +1,8 @@
+from typing import Optional
 import fire
 
 
-def convert(input_path: str, output_path: str) -> None:
+def convert(input_path: str, output_path: Optional[str] = None) -> None:
     print(f'Converting {input_path} to {output_path}.')
 
 
