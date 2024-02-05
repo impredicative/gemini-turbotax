5d48fe981b2c771e28483bca1b725d2e8298a444
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-02-05 08:58:32 -0500

    Auto-define output path

--- src/gemini_turbotax/__init__.py
+++ src/gemini_turbotax/__init__.py
@@ -1,9 +1,15 @@
+import pathlib
 from typing import Optional
+
+import pandas as pd
 import fire
 
 
 def convert(input_path: str, output_path: Optional[str] = None) -> None:
     # Ref: https://ttlc.intuit.com/turbotax-support/en-us/help-article/cryptocurrency/create-csv-file-unsupported-source/L1yhp71Nt_US_en_US
+    input_path = pathlib.Path(input_path)
+    if output_path is None:
+        output_path = input_path.parent / f'{input_path.stem}_gemini.csv'
     print(f'Converting {input_path} to {output_path}.')
 
 
