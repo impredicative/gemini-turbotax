ed9262f0c24bda719c7274e7197895657ac4c8eb
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-02-04 23:06:45 -0500

    Add Turbotax reference link

--- src/gemini_turbotax/__init__.py
+++ src/gemini_turbotax/__init__.py
@@ -3,6 +3,7 @@ import fire
 
 
 def convert(input_path: str, output_path: Optional[str] = None) -> None:
+    # Ref: https://ttlc.intuit.com/turbotax-support/en-us/help-article/cryptocurrency/create-csv-file-unsupported-source/L1yhp71Nt_US_en_US
     print(f'Converting {input_path} to {output_path}.')
 
 
